1a84bebe87c0834a958f9f34dec0bc5c32a4d177
shyne <shyne9795@gmail.com>
2020-06-25 01:55:06 +0900

    [FIX]keep screen on

--- android/app/src/main/kotlin/ja/gr/javaconf/asickohyaena/dotsite/MainActivity.kt
+++ android/app/src/main/kotlin/ja/gr/javaconf/asickohyaena/dotsite/MainActivity.kt
@@ -1,11 +1,16 @@
 package ja.gr.javaconf.asickohyaena.dotsite
 
+import android.view.WindowManager
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
